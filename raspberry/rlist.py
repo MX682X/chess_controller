ae655ab48fc57ad9b0cc2c4a8fc55f309f25c0af
allwissender <test.testinger>
2024-10-04 11:45:32 +0200

    startpos part 1

--- raspberry/rlist.py
+++ raspberry/rlist.py
@@ -30,7 +30,7 @@ def getreedlist(ser: serial.serialposix.Serial):
 
     while True:
         if ser.in_waiting != 0:
-            data = arduino.readline()
+            data = ser.readline()
             strdata = data.decode("utf-8").strip()
 
             rlist.append(strdata)
@@ -39,6 +39,31 @@ def getreedlist(ser: serial.serialposix.Serial):
     return parseR(rlist)
 
 
-arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
+def waitforstartpos(ser: serial.serialposix.Serial):
+    zuviel = []
 
-print(getreedlist(arduino))
+    startpos = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", ]
+
+    istpos = getreedlist(ser)
+
+    for i in istpos:
+        if i in startpos:
+            startpos.remove(i)
+        else:
+            zuviel.append(i)
+
+    while True:
+        if ser.in_waiting != 0:
+            data = ser.readline()
+            strdata = data.decode("utf-8").strip()
+            # print(strdata)
+            match strdata[0]:
+                case "t":
+                    pas
+                case "p":
+                    pass
+                case _:
+                    logging.warning("unkown Beginning: " + strdata[0])
