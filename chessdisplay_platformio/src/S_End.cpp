a2a7291d8c170aaee437f545c5ed9e5d52be0cdb
Allwissender <gregor.kretschmer@hm.edu>
2025-08-04 12:42:53 +0200

    remove stuff

--- chessdisplay_platformio/src/S_End.cpp
+++ chessdisplay_platformio/src/S_End.cpp
@@ -9,18 +9,6 @@ static void restart_bttn_callback(lv_event_t* e);
 
 //Endscreen
 
-
-//TODO Brauchen wir diese Funktion?
-void lv_create_s_1_2_p(lv_obj_t* parent) {
-  lv_obj_t* text_label = lv_label_create(parent);
-  lv_label_set_text(text_label, "Patt!");
-
-  lv_obj_set_style_text_align(text_label, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_set_style_text_font(text_label, &lv_font_montserrat_22, 0);
-
-  lv_obj_center(text_label);
-}
-
 void transition_s_1_2(String type) {
   if (!((type == "r") || (type == "p") || (type == "w") || (type == "b"))) {
     LV_LOG_WARN("Invalid game outcome. only b,w,r,p are allowed");
